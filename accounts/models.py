a1be3ef3be44dcc83d7d846b9c76b56b3de10beb
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-09-24 22:04:28 +0900

    add: [models.py] User attributes are created

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    GENDER_CHOICES = (("M", "Male"), ("F", "Female"))
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=20)
+    name = models.CharField(max_length=10)
+    age = models.IntegerField()
+    gender = models.CharField(choices=GENDER_CHOICES, null=True, blank=True)
+    profile_image = models.ImageField(
+        upload_to="profile_images/", null=True, blank=True
+    )
+    joined_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nickname
