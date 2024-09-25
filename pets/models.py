515a6853d04027007a26ca7a45ddc0d5994a8ee6
SHY-89 <tjduwkrn@gmail.com>
2024-09-25 11:01:17 +0900

    feat: pets
    models 생성
    1. Question: 질문내역 관리
    2. PetCaegory: 대형,중형,소형 등을 분류하여 관리
    3. PetCode: 전체적인 반려동물목록 관리
    4. AIHistory: 사용자에게 추천한 내용에 대한 관리

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Question(models.Model):
+    type = models.CharField(max_length=1, choices=[("1","사용자"), ("2","반려동물")])
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PetCategory(models.Model):
+    type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PetCode(models.Model):
+    PCID = models.ForeignKey(PetCategory, on_delete=models.CASCADE, related_name="petcodes")
+    name=models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class AIHistory(models.Model):
+    PID = models.ForeignKey(PetCode, related_name="petcodes")
+    UID = models.ForeignKey(get_user_model, related_name="users")
+    question = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
