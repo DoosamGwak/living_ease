4b885bd4cfa00ea1c257a7416ffef9ec858e7a81
SHY-89 <tjduwkrn@gmail.com>
2024-09-25 10:15:21 +0900

    add: pets app create
    1. openai 설치 및 키 셋팅
    2. pets app 생성 및 초기 셋팅

--- petmily/settings.py
+++ petmily/settings.py
@@ -28,6 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
+OPENAI_API_KEY = env("GPT_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     # project app
     "accounts",
+    "pets",
 ]
 
 MIDDLEWARE = [
--- petmily/urls.py
+++ petmily/urls.py
@@ -24,6 +24,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/accounts/", include("accounts.urls")),
+    path("api/v1/pets/", include("pets.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
