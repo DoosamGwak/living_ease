54653b3b82d6f55ad04aa271a9423b4fe54aeea1
SHY-89 <tjduwkrn@gmail.com>
2024-09-25 12:12:59 +0900

    feat: pets-question
    1. serializer 생성
    2. urls endpoint 등록
    3. 정상적으로 조회 확인

---
+++ pets/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Question
+
+class QuestionSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+
+    def get_type(self, obj):
+        return obj.get_type_display()
+    
+    class Meta:
+        model = Question
+        fields = ("type", "title", "content", )
+
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Question
+from .serializers import QuestionSerializer
 
 # Create your views here.
+class QuestionListView(ListAPIView):
+    queryset = Question.objects.all().order_by("type","title")
+    serializer_class = QuestionSerializer
