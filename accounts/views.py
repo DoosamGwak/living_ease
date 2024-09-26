2880f8433964e6c86a2731109428067be1bd9f0a
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-09-26 14:38:23 +0900

    update: 필요없는 code 몇가지  삭제

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from .serializers import (
     UserSerializer,
     UserPofileSerializer,
     UserDeleteSerializer,
-    PasswordChangeSerializer
+    PasswordChangeSerializer,
 )
 from .models import User
 from rest_framework import status
@@ -84,12 +84,7 @@ class ChangePasswordView(UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = PasswordChangeSerializer
 
-
-    def get_object(self):
-        return self.request.user
-    
     def update(self, request, *args, **kwargs):
-        self.object = self.get_object()
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
@@ -99,8 +94,14 @@ class ChangePasswordView(UpdateAPIView):
             if new_password == check_pasword:
                 user.set_password(new_password)
                 user.save()
-
-                return Response({"msg": "비밀번호가 변경되었습니다"}, status=status.HTTP_200_OK)
-
-            return Response({"error": "비밀번호가 일치하지 않습니다"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error": "비밀번호변경에 실패하였습니다"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"msg": "비밀번호가 변경되었습니다"}, status=status.HTTP_200_OK
+                )
+            return Response(
+                {"error": "비밀번호가 일치하지 않습니다"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return Response(
+            {"error": "비밀번호변경에 실패하였습니다"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
