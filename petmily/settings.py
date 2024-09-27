b761e433c62633cfda4f67dcb0f2658388edcc59
SHY-89 <tjduwkrn@gmail.com>
2024-09-27 19:25:56 +0900

    release: v1.0.0

--- petmily/settings.py
+++ petmily/settings.py
@@ -33,7 +33,7 @@ OPENAI_API_KEY = env("GPT_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","localhost","172.31.9.218"]
 
 
 # Application definition
