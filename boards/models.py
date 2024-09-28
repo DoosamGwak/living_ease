226469d8a45ea31db0df48854d343bd590612482
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-09-28 21:29:32 +0900

    update: boards app에  model 생성

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Board(TimeStampedModel):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="boards")
+    
+    def __str__(self):
+        return self.title
+    
+
+class BoardImage(TimeStampedModel):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageFieldField(upload_to="boards/", null=True, blank=True)
+    
+    def __str__(self):
+        return f"Image for {self.board.title}"
