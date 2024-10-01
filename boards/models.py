f4c0c6b85a6b96f2527dd5e8ae3ffef50b56a5f7
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-10-01 19:42:01 +0900

    update: Category모델에 자기참조 foreinkey 생성

--- boards/models.py
+++ boards/models.py
@@ -8,10 +8,17 @@ class TimeStampedModel(models.Model):
 
     class Meta:
         abstract = True
-        
+
 
 class Category(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="childcategories",
+    )
 
     def __str__(self):
         return self.name
@@ -21,7 +28,9 @@ class Board(TimeStampedModel):
     title = models.CharField(max_length=30)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="boards")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="boards")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="boards"
+    )
 
     def __str__(self):
         return self.title
