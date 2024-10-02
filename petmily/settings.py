01b489b57b260e4dcf0bf3ba36d2dcf2b46b4849
SHY-89 <tjduwkrn@gmail.com>
2024-10-02 15:36:46 +0900

    add: AWS-S3 연동
    1. S3를 사용하기 위한 초기 셋팅
    2. 셋팅에 사용한 패키지 최신화

--- petmily/settings.py
+++ petmily/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    'corsheaders',
+    'storages',
     # project app
     "accounts",
     "pets",
@@ -155,13 +157,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")  # 생성한 버킷 이름
+AWS_REGION = env("AWS_REGION")  # AWS서버의 지역
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")  # 액서스 키 PW
 
-# Media files
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# 버킷이름.s3.AWS서버지역.amazonaws.com 형식
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+# Static Setting
+STATIC_URL = "/static/"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# Media Setting
+
+MEDIA_URL = "/meida/"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
