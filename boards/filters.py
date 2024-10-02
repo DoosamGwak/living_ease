a9e91bf3c0405e652e6286b33c10d7c8a12b9312
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-10-02 20:05:42 +0900

    add: custom filterset 작성

---
+++ boards/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+from .models import Board
+
+
+
+class BoardFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', label='Search Title')
+    content = django_filters.CharFilter(field_name='content', lookup_expr='icontains', label='Search Content')
+    username = django_filters.CharFilter(field_name='user__username', lookup_expr='icontains', label='Search Username')
+
+    class Meta:
+        model = Board
+        fields = ['title', 'content', 'username']
