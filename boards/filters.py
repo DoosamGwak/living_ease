d7d095c00bafbc28c6dc2d2aa9e4a0c255756bcf
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-10-04 10:46:46 +0900

    update: Notice 전용 filterset

--- boards/filters.py
+++ boards/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Board
+from .models import Board, NoticeBoard
 
 
 
@@ -10,4 +10,14 @@ class BoardFilter(django_filters.FilterSet):
 
     class Meta:
         model = Board
+        fields = ['title', 'content', 'username']
+
+
+class NoticeBoardFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', label='Search Title')
+    content = django_filters.CharFilter(field_name='content', lookup_expr='icontains', label='Search Content')
+    username = django_filters.CharFilter(field_name='user__username', lookup_expr='icontains', label='Search Username')
+
+    class Meta:
+        model = NoticeBoard
         fields = ['title', 'content', 'username']
