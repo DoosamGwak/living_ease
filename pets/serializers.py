c03368c938bb3e86f169b7a7d9263be578ce33a0
SHY-89 <tjduwkrn@gmail.com>
2024-10-04 10:48:01 +0900

    feat: feat/pets-question-choice
    설문에 대한 답변이 같이 나올 수 있도록 serializer 수정

--- pets/serializers.py
+++ pets/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
-from .models import Question
+from .models import Question, Choice
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    content_type = serializers.SerializerMethodField()
+    
+    def get_content_type(self, obj):
+        return obj.get_content_type_display()
+    
+    class Meta:
+        model = Choice
+        fields = ("pk", "content_type", "content")
+    
 
 class QuestionSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
+    answer = serializers.SerializerMethodField()
 
     def get_type(self, obj):
         return obj.get_type_display()
     
+    def get_answer(self, obj):
+        serializer = ChoiceSerializer(obj.choices, many=True)
+        serializer.bind('', self)
+        return serializer.data
+    
     class Meta:
         model = Question
-        fields = ("pk", "type", "title", "content", )
-
+        fields = ("pk", "type", "title", "content", "answer")
