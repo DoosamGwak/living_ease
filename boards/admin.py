967ac912102f564eae5591eab6db8b5d39d90553
DESKTOP-NK05NPD\User <gyum9779@naver.com>
2024-10-07 10:38:48 +0900

    fix: boards,noticeboards,boardimage admin에 추가

--- boards/admin.py
+++ boards/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Board, NoticeBoard, BoardImage
+
 
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["name"]
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NoticeBoard)
+class NoticeBoardAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BoardImage)
+class BoardImageAdmin(admin.ModelAdmin):
+    pass
