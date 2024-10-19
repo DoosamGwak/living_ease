bd35ce4fc5e38dd6c85e04e2020be4014e18e8eb
Seungju_Yi <yi77371463@gmail.com>
2024-10-20 07:27:59 +0900

    fix: community post view 수정

--- boards/views.py
+++ boards/views.py
@@ -26,23 +26,26 @@ def get_category(category_name):
     except Category.DoesNotExist:
         raise NotFound("요청한 페이지가 없습니다.")
 
+
 class CommunityListAPIView(ListCreateAPIView):
     serializer_class = CommunityListSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = CommunityBoardFilter
-    
+
     def get_queryset(self):
         community_category = Category.objects.get(name="community")
         child_categories = community_category.childcategories.all()
-        return Board.objects.filter(category__in=child_categories).order_by("-created_at")
+        return Board.objects.filter(category__in=child_categories).order_by(
+            "-created_at"
+        )
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = CommunityCreateSerializer
-        return super().post(request, *args, **kwargs)
-    
+        return self.post(request, *args, **kwargs)
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-    
+
 
 class TipListAPIView(ListCreateAPIView):
     serializer_class = BoardListSerializer
@@ -52,7 +55,6 @@ class TipListAPIView(ListCreateAPIView):
     def get_queryset(self):
         childcategory = get_category("tip")
         return childcategory.boards.all().order_by("-id")
-        
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = BoardCreateSerializer
@@ -108,7 +110,6 @@ class VaccineListAPIView(ListCreateAPIView):
     def get_queryset(self):
         childcategory = get_category("vaccine")
         return childcategory.boards.all().order_by("-id")
-        
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = BoardCreateSerializer
@@ -128,7 +129,6 @@ class TrainingListAPIView(ListCreateAPIView):
     def get_queryset(self):
         childcategory = get_category("training")
         return childcategory.boards.all().order_by("-id")
-        
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = BoardCreateSerializer
@@ -148,7 +148,6 @@ class HealthyfoodListAPIView(ListCreateAPIView):
     def get_queryset(self):
         childcategory = get_category("healthyfood")
         return childcategory.boards.all().order_by("-id")
-        
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = BoardCreateSerializer
@@ -168,7 +167,6 @@ class SuppliesListAPIView(ListCreateAPIView):
     def get_queryset(self):
         childcategory = get_category("supplies")
         return childcategory.boards.all().order_by("-id")
-        
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = BoardCreateSerializer
@@ -188,18 +186,20 @@ class NoticeListAPIView(ListCreateAPIView):
     def get_queryset(self):
         childcategory = get_category("notice")
         return NoticeBoard.objects.filter(category=childcategory).order_by("priority")
-        
+
     def post(self, request, *args, **kwargs):
         self.serializer_class = NoticeCreateSerializer
-        priority = request.data.get('priority')
+        priority = request.data.get("priority")
         if int(priority) < 1:
-            raise ValidationError({"detail":"우선순위 값은 1부터 입력 가능합니다."})
+            raise ValidationError({"detail": "우선순위 값은 1부터 입력 가능합니다."})
         return super().post(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         category = get_category("notice")
         priority = serializer.validated_data.get("priority", 1)
-        existing_boards = NoticeBoard.objects.filter(priority=priority, category=category)
+        existing_boards = NoticeBoard.objects.filter(
+            priority=priority, category=category
+        )
         for board in existing_boards:
             board.priority += 1
             board.save()
@@ -215,7 +215,7 @@ class FaqListAPIView(ListCreateAPIView):
     def get_queryset(self):
         childcategory = get_category("faq")
         return childcategory.boards.all().order_by("-id")
-        
+
     def post(self, request, *args, **kwargs):
         self.serializer_class = BoardCreateSerializer
         return super().post(request, *args, **kwargs)
@@ -234,7 +234,6 @@ class HowtouseListAPIView(ListCreateAPIView):
     def get_queryset(self):
         childcategory = get_category("howtouse")
         return childcategory.boards.all().order_by("-id")
-        
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = BoardCreateSerializer
@@ -249,12 +248,11 @@ class DirectmsgListAPIView(ListCreateAPIView):
     serializer_class = BoardListSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = BoardFilter
-    permission_classes=[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         childcategory = get_category("directmsg")
         return childcategory.boards.filter(user=self.request.user).order_by("-id")
-        
 
     def post(self, request, *args, **kwargs):
         self.serializer_class = BoardCreateSerializer
@@ -269,9 +267,9 @@ class BoardDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardDetailSerializer
     lookup_field = "pk"
-    
+
     def get_serializer(self, *args, **kwargs):
-        kwargs['partial'] = True
+        kwargs["partial"] = True
         return super(BoardDetailAPIView, self).get_serializer(*args, **kwargs)
 
     def get_object(self):
@@ -279,19 +277,19 @@ class BoardDetailAPIView(RetrieveUpdateDestroyAPIView):
         try:
             board = Board.objects.get(pk=board_pk)
             if board.category.pk == 14 and board.user != self.request.user:
-                raise PermissionDenied({"detail":"이 게시글을 볼 권한이 없습니다."})
+                raise PermissionDenied({"detail": "이 게시글을 볼 권한이 없습니다."})
             return board
         except Board.DoesNotExist:
-            raise NotFound({"detail":"요청한 게시글이 없습니다."})
+            raise NotFound({"detail": "요청한 게시글이 없습니다."})
 
     def perform_update(self, serializer):
         instance = self.get_object()
         if instance.user != self.request.user:
-            raise PermissionDenied({"detail":"이 게시글을 수정할 권한이 없습니다."})
+            raise PermissionDenied({"detail": "이 게시글을 수정할 권한이 없습니다."})
 
     def perform_destroy(self, instance):
         if instance.user != self.request.user:
-            raise PermissionDenied({"detail":"이 게시글을 삭제할 권한이 없습니다."})
+            raise PermissionDenied({"detail": "이 게시글을 삭제할 권한이 없습니다."})
         instance.delete()
 
 
@@ -299,9 +297,9 @@ class NoticeDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = NoticeBoard.objects.all()
     serializer_class = NoticeDetailSerializer
     lookup_field = "noticeboard_pk"
-    
+
     def get_serializer(self, *args, **kwargs):
-        kwargs['partial'] = True
+        kwargs["partial"] = True
         return super(NoticeDetailAPIView, self).get_serializer(*args, **kwargs)
 
     def get_object(self):
@@ -309,16 +307,16 @@ class NoticeDetailAPIView(RetrieveUpdateDestroyAPIView):
         try:
             return NoticeBoard.objects.get(pk=board_pk)
         except NoticeBoard.DoesNotExist:
-            raise NotFound({"detail":"요청한 공지사항이 없습니다."})
+            raise NotFound({"detail": "요청한 공지사항이 없습니다."})
 
     def perform_update(self, serializer):
         instance = self.get_object()
         if instance.user != self.request.user:
-            raise PermissionDenied({"detail":"이 공지사항을 수정할 권한이 없습니다."})
+            raise PermissionDenied({"detail": "이 공지사항을 수정할 권한이 없습니다."})
 
     def perform_destroy(self, instance):
         if instance.user != self.request.user:
-            raise PermissionDenied({"detail":"이 공지사항을 삭제할 권한이 없습니다."})
+            raise PermissionDenied({"detail": "이 공지사항을 삭제할 권한이 없습니다."})
         instance.delete()
 
 
@@ -334,13 +332,15 @@ class CommentListAPIView(ListCreateAPIView):
         try:
             return Board.objects.get(pk=board_pk)
         except Board.DoesNotExist:
-            raise NotFound({"detail":"요청한 게시글이 없습니다"})
+            raise NotFound({"detail": "요청한 게시글이 없습니다"})
 
     def perform_create(self, serializer):
         board_pk = self.kwargs.get("board_pk")
         board = self.get_object()
         if board.category.parent and board.category.parent.pk == 3:
-            raise PermissionDenied({"detail":"이 게시글에는 댓글을 작성할 수 없습니다."})
+            raise PermissionDenied(
+                {"detail": "이 게시글에는 댓글을 작성할 수 없습니다."}
+            )
         comment_author = self.request.user
         serializer.save(board=board, user=comment_author)
 
@@ -348,9 +348,9 @@ class CommentListAPIView(ListCreateAPIView):
 class CommentDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = CommentSerializer
     lookup_field = "pk"
-    
+
     def get_serializer(self, *args, **kwargs):
-        kwargs['partial'] = True
+        kwargs["partial"] = True
         return super(CommentDetailAPIView, self).get_serializer(*args, **kwargs)
 
     def get_queryset(self):
@@ -362,15 +362,15 @@ class CommentDetailAPIView(RetrieveUpdateDestroyAPIView):
         try:
             return Comment.objects.get(pk=comment_pk)
         except Comment.DoesNotExist:
-            raise NotFound({"detail":"요청한 댓글이 없습니다."})
+            raise NotFound({"detail": "요청한 댓글이 없습니다."})
 
     def perform_update(self, serializer):
         instance = self.get_object()
         if instance.user != self.request.user:
-            raise PermissionDenied({"detail":"이 댓글을 수정할 권한이 없습니다."})
+            raise PermissionDenied({"detail": "이 댓글을 수정할 권한이 없습니다."})
         serializer.save()
 
     def perform_destroy(self, instance):
         if instance.user != self.request.user:
-            raise PermissionDenied({"detail":"이 댓글을 삭제할 권한이 없습니다."})
+            raise PermissionDenied({"detail": "이 댓글을 삭제할 권한이 없습니다."})
         instance.delete()
